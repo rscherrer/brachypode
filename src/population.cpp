3a503c59a4546a5dc76e1359faf2d63d770e6f3d
Raphaël Scherrer <raphael.scherrer@evobio.eu>
2025-04-04 16:14:31 +0200

    EOD

--- src/population.cpp
+++ src/population.cpp
@@ -351,6 +351,10 @@ void Population::cycle(Printer &print) {
     // Check
     assert(!individuals->empty());
 
+    double meanfit = 0.0;
+    size_t nhigh = 0u;
+    double meanr = 0.0;
+
     // For each individual...
     for (Individual& ind : *individuals) {
 
@@ -391,6 +395,19 @@ void Population::cycle(Printer &print) {
 
         // Check
         assert(fitness >= 0.0);
+        //assert(fitness <= std::exp(r));
+
+        meanfit += fitness;
+        if (fitness > 1E6) ++nhigh; 
+        meanr += r;
+
+        if (fitness > 1E6) {
+
+            std::cout << r << '\n';
+            std::cout << n << '\n';
+            std::cout << Ktot << '\n';
+
+        }
 
         // Realized number of seeds
         const size_t nseeds = rnd::poisson(fitness)(rnd::rng);
@@ -402,6 +419,14 @@ void Population::cycle(Printer &print) {
         totseeds += nseeds;
 
     }
+
+    meanfit /= popsize;
+    meanr /= popsize;
+
+    //std::cout << totseeds << '\n';
+    //std::cout << meanfit << '\n';
+    //std::cout << nhigh << '\n';
+    //std::cout << meanr << '\n';
 
     // Prepare space to welcome newborns
     newborns->reserve(totseeds);
